aa33e16b54f70041c6433a54957a06054087d321
Anthony Martinet <anthony@alkivi.fr>
2019-08-22 13:32:42 +0200

    fix(price): we raised from 4.2 to 5 the meal price

--- louisebot/db.py
+++ louisebot/db.py
@@ -43,7 +43,7 @@ class Day(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     date = Column(Date, nullable=False, default=datetime.date.today())
-    price = Column(Float, nullable=False, default=4.2)
+    price = Column(Float, nullable=False, default=5)
     presences = relationship("Presence", back_populates="day")
 
 class Expense(Base):
